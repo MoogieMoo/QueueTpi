8f3526ac2bf8b9a0077ac99a3aac4ee39041336a
adrisj7 <adrisj7@gmail.com>
2017-04-03 08:34:57 -0400

    Remove unused methods in Deque interface

--- Deque.java
+++ Deque.java
@@ -8,13 +8,9 @@
 public interface Deque<Quasar> {
 
     public void addFirst(Quasar e);
-    //public void offerFirst(Quasar e);
     public void addLast(Quasar e);
-    //public boolean offerLast(Quasar e);
     public Quasar removeFirst();	
-    //public Quasar pollFirst();
     public Quasar removeLast();
-    //public Quasar pollLast();	
     public Quasar peekFirst();	
     public Quasar peekLast();
 
