13c3ce8c1a9f7adc4019a27b0e7ac23021713954
Yi Xia <yxia1@stuy.edu>
2017-04-04 15:50:16 -0400

    update driver file

--- Driver.java
+++ Driver.java
@@ -1,15 +1,24 @@
+// Maggie Xia
+// APCS2 pd1
+// HW29--Stress is the Best
+// 2017-04-04
+
+//Driver class
 public class Driver {
     
     public static void main ( String[] args ) {
 
+	//Instantiate new Deque
 	Deque<String> test1 = new ALDeque<String>();
 	
 	System.out.println("\nTesting enqueue() from front end...");
 	q.addFirst("uno");
 	q.addFirst("dos");
 	q.addFirst("tres");
-	System.out.println(q);//show how it looks at this point
+	//should print: tres dos uno
+	System.out.println(q);
 
+	//should print: yi er san
 	System.out.println("\nTesting enqueue() from back end...");
 	q.addLast("yi");
 	q.addLast("er");
@@ -18,25 +27,25 @@ public class Driver {
      
 	System.out.println("\nTesting get() methods...");
 	System.out.println( "First: " );
-	System.out.println( q.getFirst() );
+	System.out.println( q.getFirst() );//tres
 	System.out.println( "Last: " );
-	System.out.println( q.getLast() );
+	System.out.println( q.getLast() );//san
 	
 	System.out.println("\nnow dequeuing from front end...");
 	System.out.println( "First: " );
-	System.out.println( q.removeFirst() );
+	System.out.println( q.removeFirst() );//tres
 	System.out.println( "Second: " );
-	System.out.println( q.removeFirst() );
+	System.out.println( q.removeFirst() );//dos
 	System.out.println( "Third: " );
-	System.out.println( q.removeFirst() );
+	System.out.println( q.removeFirst() );//uno
 
 	System.out.println("\nnow dequeuing from back end...");
 	System.out.println( "First: " );
-	System.out.println( q.removeLast() );
+	System.out.println( q.removeLast() );//san
 	System.out.println( "Second: " );
-	System.out.println( q.removeLast() );
+	System.out.println( q.removeLast() );//er
 	System.out.println( "Third: " );
-	System.out.println( q.removeLast() );
+	System.out.println( q.removeLast() );//yi
 
     }//end main method
 
